4dfa5ba1fcab86e39e313de84e82cbc5cefb8041
Lucas Sciancalepore <lucas.sciancalepore91@gmail.com>
2019-09-11 18:21:26 -0300

    Unidad 2 hasta if - else(if-else)

--- Unidad2.cs
+++ Unidad2.cs
@@ -22,8 +22,14 @@ public class Ejemplo_2_1
           Console.WriteLine("El número es mayor a 0");
         if (numero == 0)
           Console.WriteLine("El número no es mayor a 0");
-*/
+
           if (numero <= 10)
             Console.WriteLine("El número es menor o igual a 10");
+          else
+            if (numero < 5)
+              Console.WriteLine("El número es menor a 5");
+            else
+              Console.WriteLine("Ya me perdí qué número soy");
+*/              
     }
 }
