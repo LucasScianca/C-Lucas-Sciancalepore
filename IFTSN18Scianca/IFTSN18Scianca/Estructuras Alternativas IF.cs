2be423f45184572ec71160c0ed4b06bd8c749792
Lucas <lucas.sciancalepore91@gmail.com>
2019-09-13 18:06:37 -0300

    IFs

---
+++ IFTSN18Scianca/IFTSN18Scianca/Estructuras Alternativas IF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IFTSN18Scianca
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numero;
+            Console.WriteLine("Por favor, ingrese un número");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero > 0)
+                Console.WriteLine("El número es positivo");
+        }
+    }
+}
